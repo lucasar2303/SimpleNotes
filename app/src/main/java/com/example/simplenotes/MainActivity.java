27c7947e09a2394ee691b5e73ab24aaa061c6576
Lucas Anastácio <lucas-ar13@hotmail.com>
2022-10-04 15:14:31 -0300

    Splash + Icones + Font

--- app/src/main/java/com/example/simplenotes/MainActivity.java
+++ app/src/main/java/com/example/simplenotes/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.simplenotes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.splashscreen.SplashScreen;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
